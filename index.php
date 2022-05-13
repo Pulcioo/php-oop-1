1ca489670fe0e4be9314d353f202a77e943a0187
Davide Pulci <davide.pulci@gmail.com>
2022-05-13 17:06:27 +0200

    da stampare a schermo

--- index.php
+++ index.php
@@ -10,4 +10,36 @@
         - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
     */
 
+    class Movie{
+
+        // variabili
+        public $title;
+        public $director;
+        public $release;
+
+        // costruttore
+        function __construct($title, $director, $release) {
+            $this->title = $title;
+            $this->director = $director;
+            $this->release = $release;
+        }
+
+        // metodi
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function getDirector(){
+            return $this->director;
+        }
+
+        public function getRelease(){
+            return $this->release;
+        }
+    }
+
+    $movie_1 = new Movie('Fight Club', 'David Fincher', '29-10-1999');
+    $movie_2 = new Movie('Il buono, il brutto, il cattivo', 'Sergio Leone', '23-12-1966');
+    $movie_3 = new Movie('Pulp Fiction', 'Quentin Tarantino', '28-10-1994');
+
 ?>
